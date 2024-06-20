b49513e4a25f593ed7b391e001b7522949476a10
Sulakshani99 <139636218+Sulakshani99@users.noreply.github.com>
2024-06-20 15:09:27 +0530

    Create responsive vehicle search

--- frontend/src/components/Search/VehicleSearch.jsx
+++ frontend/src/components/Search/VehicleSearch.jsx
@@ -30,7 +30,7 @@ const VehicleSearch = () => {
 
     try {
       const response = await axios.post(
-       "http://localhost:3001/api/v1/vehicles/retrieveVehicles/${token}",
+        "http://localhost:3001/api/v1/vehicles/retrieveVehicles/${token}",
         searchParams,
         {
           headers: {
@@ -57,17 +57,17 @@ const VehicleSearch = () => {
 
   return (
     <section className="become__driver" style={{ marginTop: "100px", background: "#f0f0f8" }}>
-      <Container style={{ marginTop: "100px" }}>
+      <Container>
         <Row>
-          <Col lg="6" md="6" sm="12" style={{ marginTop: "-240px", marginLeft: "0px", color: "" }}>
+          <Col lg="6" md="6" sm="12">
             <div style={{ background: "#f9a826", padding: "10px" }}>
               <h2 className="section__title become__driver-title" style={{ marginBottom: "10px", color: "#021F61" }}>
                 Find Your Car
               </h2>
               <form onSubmit={handleSubmit}>
-                <div style={{ display: "flex", padding: "20px" }}>
+                <div style={{ display: "flex", flexWrap: "wrap" }}>
                   <select
-                    style={{ margin: "10px", padding: "10px", width: "200px" }}
+                    style={{ margin: "10px", padding: "10px", width: "200px", cursor: "pointer" }}
                     name="vehicleType"
                     value={searchParams.vehicleType}
                     onChange={handleInputChange}
@@ -80,7 +80,7 @@ const VehicleSearch = () => {
                     <option value="van">Van</option>
                   </select>
                   <input
-                    style={{ margin: "10px", padding: "10px" }}
+                    style={{ margin: "10px", padding: "10px", flex: "1", cursor: "pointer" }}
                     type="text"
                     name="brand"
                     value={searchParams.brand}
@@ -88,7 +88,7 @@ const VehicleSearch = () => {
                     placeholder="Brand"
                   />
                   <input
-                    style={{ margin: "10px", padding: "10px" }}
+                    style={{ margin: "10px", padding: "10px", flex: "1", cursor: "pointer" }}
                     type="text"
                     name="model"
                     value={searchParams.model}
@@ -96,7 +96,7 @@ const VehicleSearch = () => {
                     placeholder="Model"
                   />
                   <input
-                    style={{ margin: "10px", padding: "10px" }}
+                    style={{ margin: "10px", padding: "10px", flex: "1", cursor: "pointer" }}
                     type="text"
                     name="style"
                     value={searchParams.style}
@@ -104,7 +104,7 @@ const VehicleSearch = () => {
                     placeholder="Style"
                   />
                   <select
-                    style={{ margin: "10px", padding: "10px", width: "200px" }}
+                    style={{ margin: "10px", padding: "10px", width: "200px", cursor: "pointer" }}
                     name="fuelType"
                     value={searchParams.fuelType}
                     onChange={handleInputChange}
@@ -117,62 +117,79 @@ const VehicleSearch = () => {
                     <option value="CNG">CNG</option>
                   </select>
                   <input
-                    style={{ margin: "10px", padding: "10px" }}
+                    style={{ margin: "10px", padding: "10px", flex: "1", maxWidth: '200px', cursor: "pointer" }}
                     type="text"
                     name="manufacturedYear"
                     value={searchParams.manufacturedYear}
                     onChange={handleInputChange}
                     placeholder="Manufactured Year"
                   />
-                  <Button type="submit" color="primary" style={{ margin: "10px", padding: "10px" }}>
+                  <Button
+                    type="submit"
+                    color="primary"
+                    style={{
+                      margin: "10px",
+                      padding: "10px",
+                      width: "200px",
+                      cursor: "pointer",
+                      backgroundColor: "rgb(2 31 97)", 
+                      color: "#fff", // default text color
+                      transition: "background-color 0.3s, border-color 0.3s, color 0.3s", // smooth transition
+                    }}
+                    onMouseEnter={(e) => {
+                      e.target.style.backgroundColor = "#0056b3"; }}
+                    onMouseLeave={(e) => {
+                      e.target.style.backgroundColor = "rgb(2 31 97)"; }} >
                     Search
                   </Button>
                 </div>
               </form>
             </div>
-
-            <div className="d-flex flex-wrap">
-              <div className="selling__car-list">
-                {vehicles.map((vehicle) => (
-                  <Col lg="3" md="3" sm="6" key={vehicle.vehicleId}>
-                    <div key={vehicle.vehicleId}>
-                      <div className="searching___car-list" style={{}}>
-                        <div className="car__item" style={{ width: "250px" }}>
-                          <div className="car__image">
-                            {vehicle.album.length > 0 && (
-                              <img src={vehicle.album[0].photoURL} alt={`Image 1`} className="w-100" />
-                            )}
-                          </div>
-
-                          <div className="car__item-content mt-4">
-                            <h4 className="section__title text-center">{vehicle.brand}</h4>
-
-                            <div className="car__item-info car--details d-flex align-items-center justify-content-between align-baseline mt-3 mb-4 ml-3 mr-10">
-                              <span className="d-flex align-items-center gap-10">
-                                <i className="ri-car-line"></i>
-                                {vehicle.model}
-                              </span>
-                              <span className="d-flex align-items-center gap-10">
-                                <i className="ri-settings-2-line"></i>
-                                {vehicle.fuelType}
-                              </span>
-                              <span className="d-flex align-items-center gap-10">
-                                <i className="ri-timer-flash-line"></i>
-                                {vehicle.manufacturedCountry}
-                              </span>
-                            </div>
-                          </div>
-                        </div>
-                      </div>
-                    </div>
-                  </Col>
-                ))}
-              </div>
-            </div>
-
-            {error && <p>{error}</p>}
           </Col>
         </Row>
+
+        <Row>
+        <div className="selling1__car-list">
+          {vehicles.map((vehicle) => (
+            <Col key={vehicle.vehicleId} lg="3" md="4" sm="6">
+              <div className="searching___car-list">
+                <div className="car__item" style={{ width: "250px", margin: "10px" }}>
+                  <div className="car__image">
+                    {vehicle.album.length > 0 && (
+                      <img src={vehicle.album[0].photoURL} alt={`Image 1`} className="w-100" />
+                    )}
+                  </div>
+                  <div className="car__item-content mt-4">
+                    <h4 className="section__title text-center">{vehicle.brand}</h4>
+                    <div className="car__item-info car--details d-flex align-items-center justify-content-between align-baseline mt-3 mb-4 ml-3 mr-10">
+                      <span className="d-flex align-items-center gap-10">
+                        <i className="ri-car-line"></i>
+                        {vehicle.model}
+                      </span>
+                      <span className="d-flex align-items-center gap-10">
+                        <i className="ri-settings-2-line"></i>
+                        {vehicle.fuelType}
+                      </span>
+                      <span className="d-flex align-items-center gap-10">
+                        <i className="ri-timer-flash-line"></i>
+                        {vehicle.manufacturedCountry}
+                      </span>
+                    </div>
+                  </div>
+                </div>
+              </div>
+            </Col>
+          ))}
+          </div>
+        </Row>
+
+        {error && (
+          <Row>
+            <Col lg="6" md="6" sm="12">
+              <p>{error}</p>
+            </Col>
+          </Row>
+        )}
       </Container>
     </section>
   );
--- frontend/src/pages/OrderDetails.jsx
+++ frontend/src/pages/OrderDetails.jsx
@@ -2,25 +2,24 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import Helmet from '../components/Helmet/Helmet';
+import '../styles/order-details.css';
 
 const OrderDetails = () => {
-    const {slug} = useParams();
+  const { slug } = useParams();
   const [order, setOrder] = useState(null);
   const [status, setStatus] = useState("");
-  // const [message, setMessage] = useState("");
+  const [newMessage, setNewMessage] = useState("");
   const [loading, setLoading] = useState(true);
   const [error, setError] = useState(null);
-  const [newMessage, setNewMessage] = useState("");
 
   useEffect(() => {
     const fetchOrderDetails = async () => {
       try {
         const response = await axios.get(`http://localhost:3001/api/v1/orders/retrieveOneOrder/${slug}`);
         const orderData = response.data;
-        // Format the orderedDate for the datetime-local input
         const formattedDate = new Date(orderData.orderedDate).toISOString().slice(0, 16);
         setOrder({ ...orderData, orderedDate: formattedDate });
-        setStatus(orderData.status); // Set current status
+        setStatus(orderData.status);
         setLoading(false);
       } catch (error) {
         setError("Failed to fetch order details. Please try again later.");
@@ -34,7 +33,6 @@ const OrderDetails = () => {
   const handleSave = async (e) => {
     e.preventDefault();
     try {
-      // Update order status and message
       const updatedOrder = { ...order, status };
       if (newMessage) {
         updatedOrder.chatBox.push({ message: newMessage, owner: 'owner' });
@@ -62,128 +60,82 @@ const OrderDetails = () => {
   }
 
   return (
-    // <div>
-    //   <h2 style={{marginTop:'100px'}}>Order Details</h2>
-      
-    //   <p>Customer ID : {order.customerID} </p>
-    //   <p>Customer Name : {order.customerName}</p>
-    //   <p>Customer Email : {order.customerEmail}</p>
-    //   {order.items.map((item, index) => (
-    //     <div key={index}>
-    //     <p style={{ fontWeight:'bold'}}> Vehicle {index + 1}:</p>
-    //     <p>Vehicle Brand: {item.vehicleBrand}</p>
-    //     <p>Vehicle Model: {item.vehicleModel}</p>
-    //     <p>Vehicle Price Range: {item.vehiclePriceRange}</p>
-    //     <p>Vehicle Type: {item.vehicleType}</p>
-    //     <p>Vehicle Color: {item.vehicleColor}</p>
-    //     <p>Quantity: {item.quantity}</p>
-    //     </div>
-    //   ))}
-
-    //    <div style={{display:'inline-flex', gap:'20px'}}>
-    //    <p>Status:</p>
-    //   <select value={status} onChange={(e) => setStatus(e.target.value)}>
-    //     <option value="pending">Pending</option>
-    //     <option value="processing">Processing</option>
-    //     <option value="shipped">Shipped</option>
-    //     <option value="delivered">Delivered</option>
-    //     <option value="cancelled">Cancelled</option>
-    //   </select>
-    //    </div>
-
-    //    <div>
-    //    <div style={{display:'inline-flex', gap:'20px'}}>
-    //    <p>Message:</p>
-    //   <textarea value={message} onChange={(e) => setMessage(e.target.value)} />
-    //    </div>
-    //    </div>
-       
-    //   <br /> 
-    //   <button onClick={handleSave}>Save</button>
-    // </div>
-
     <Helmet title="Order Details">
       <div className="flex items-center justify-center my-10" style={{ marginTop: '100px' }}>
         <div className="main1">
           <div className="register">
             <form className="form">
-              <div style={{ maxWidth: '1600px', margin: '0 auto', padding: '20px', border: '1px solid #fff', borderRadius: '5px', marginTop: '10px' }}>
-                <h2 style={{ marginBottom: '20px', fontSize: '24px', fontWeight: 'bold', color: '#233c5c', textAlign: 'center' }}>Order Details</h2>
+              <div className="order-details-container">
+                <h2 className="order-details-header">Order Details</h2>
                 <h4>Customer Information</h4>
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-                  {/* <div style={{ width: '48%' }}>
-                    <label htmlFor="customerID">Customer ID</label>
-                    <input type="text" name="customerID" value={order.customerID} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
-                  </div> */}
                   <div style={{ width: '48%' }}>
-                    <label htmlFor="customerName">Customer Name</label>
-                    <input type="text" name="customerName" value={order.customerName} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                    <label className="order-details-label" htmlFor="customerName">Customer Name</label>
+                    <input className="order-details-input" type="text" name="customerName" value={order.customerName} readOnly />
                   </div>
                   <div style={{ width: '48%' }}>
-                    <label htmlFor="customerEmail">Customer Email</label>
-                    <input type="text" name="customerEmail" value={order.customerEmail} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
-                  </div>
-                </div>
-                <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-                <div style={{ width: '48%' }}>
-                    <label htmlFor="orderedDate">Ordered Date</label>
-                    <input type="datetime-local" name="orderedDate" value={order.orderedDate} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
-                  </div>
-                  <div style={{ width: '48%' }}>
-                    <label htmlFor="customerMobileNumber">Customer Mobile Number</label>
-                    <input type="text" name="customerMobileNumber" value={order.customerMobileNumber} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                    <label className="order-details-label" htmlFor="customerEmail">Customer Email</label>
+                    <input className="order-details-input" type="text" name="customerEmail" value={order.customerEmail} readOnly />
                   </div>
                 </div>
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
                   <div style={{ width: '48%' }}>
-                    <label htmlFor="customerAddress">Customer Address</label>
-                    <input type="text" name="customerAddress" value={order.customerAddress} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                    <label className="order-details-label" htmlFor="orderedDate">Ordered Date</label>
+                    <input className="order-details-input" type="datetime-local" name="orderedDate" value={order.orderedDate} readOnly />
+                  </div>
+                  <div style={{ width: '48%' }}>
+                    <label className="order-details-label" htmlFor="customerMobileNumber">Customer Mobile Number</label>
+                    <input className="order-details-input" type="text" name="customerMobileNumber" value={order.customerMobileNumber} readOnly />
+                  </div>
+                </div>
+                <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
+                  <div style={{ width: '48%' }}>
+                    <label className="order-details-label" htmlFor="customerAddress">Customer Address</label>
+                    <input className="order-details-input" type="text" name="customerAddress" value={order.customerAddress} readOnly />
                   </div>
                 </div>
 
                 <h4>Vehicle Details</h4>
                 {order.items.map((item, index) => (
                   <div key={index}>
-                    {/* <h3 style={{ fontWeight: 'bold' }}>Vehicle {index + 1}:</h3> */}
                     <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
                       <div style={{ width: '48%' }}>
-                        <label htmlFor={`vehicleBrand${index}`}>Vehicle Brand</label>
-                        <input type="text" name={`vehicleBrand${index}`} value={item.vehicleBrand} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                        <label className="order-details-label" htmlFor={`vehicleBrand${index}`}>Vehicle Brand</label>
+                        <input className="order-details-input" type="text" name={`vehicleBrand${index}`} value={item.vehicleBrand} readOnly />
                       </div>
                       <div style={{ width: '48%' }}>
-                        <label htmlFor={`vehicleModel${index}`}>Vehicle Model</label>
-                        <input type="text" name={`vehicleModel${index}`} value={item.vehicleModel} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                        <label className="order-details-label" htmlFor={`vehicleModel${index}`}>Vehicle Model</label>
+                        <input className="order-details-input" type="text" name={`vehicleModel${index}`} value={item.vehicleModel} readOnly />
                       </div>
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
                       <div style={{ width: '48%' }}>
-                        <label htmlFor={`vehiclePriceRange${index}`}>Vehicle Price Range</label>
-                        <input type="text" name={`vehiclePriceRange${index}`} value={item.vehiclePriceRange} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                        <label className="order-details-label" htmlFor={`vehiclePriceRange${index}`}>Vehicle Price Range (Rs)</label>
+                        <input className="order-details-input" type="text" name={`vehiclePriceRange${index}`} value={item.vehiclePriceRange} readOnly />
                       </div>
                       <div style={{ width: '48%' }}>
-                        <label htmlFor={`vehicleType${index}`}>Vehicle Type</label>
-                        <input type="text" name={`vehicleType${index}`} value={item.vehicleType} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                        <label className="order-details-label" htmlFor={`vehicleType${index}`}>Vehicle Type</label>
+                        <input className="order-details-input" type="text" name={`vehicleType${index}`} value={item.vehicleType} readOnly />
                       </div>
                     </div>
                     <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
                       <div style={{ width: '48%' }}>
-                        <label htmlFor={`vehicleColor${index}`}>Vehicle Color</label>
-                        <input type="text" name={`vehicleColor${index}`} value={item.vehicleColor} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                        <label className="order-details-label" htmlFor={`vehicleColor${index}`}>Vehicle Color</label>
+                        <input className="order-details-input" type="text" name={`vehicleColor${index}`} value={item.vehicleColor} readOnly />
                       </div>
                       <div style={{ width: '48%' }}>
-                        <label htmlFor={`quantity${index}`}>Quantity</label>
-                        <input type="number" name={`quantity${index}`} value={item.quantity} readOnly style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }} />
+                        <label className="order-details-label" htmlFor={`quantity${index}`}>Quantity</label>
+                        <input className="order-details-input" type="text" name={`quantity${index}`} value={item.quantity} readOnly />
                       </div>
                     </div>
                   </div>
                 ))}
 
-                <h4>Order Status</h4>
+                <h4>Update Order Status</h4>
                 <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
                   <div style={{ width: '48%' }}>
-                    <label htmlFor="status">Status</label>
-                    <select name="status" value={status} onChange={(e) => setStatus(e.target.value)} style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }}>
-                      {/* <text value="select status">Select Status</text> */}
+                    <label className="order-details-label" htmlFor="status">Status</label>
+                    <select className="order-details-select" value={status} onChange={(e) => setStatus(e.target.value)}>
                       <option value="pending">Pending</option>
                       <option value="processing">Processing</option>
                       <option value="shipped">Shipped</option>
@@ -193,10 +145,8 @@ const OrderDetails = () => {
                   </div>
                 </div>
 
-                <div style={{ display: 'flex', justifyContent: 'space-between', marginBottom: '20px' }}>
-                <div style={{ marginBottom: '20px', width: '100%' }}>
-                  <h4>Messages</h4>
-                  <div style={{ maxHeight: '300px', overflowY: 'auto', border: '1px solid #ccc', padding: '10px', borderRadius: '5px', marginBottom: '10px' }}>
+                <h4>Chat Box</h4>
+                <div style={{ maxHeight: '300px', overflowY: 'auto', border: '1px solid #ccc', padding: '10px', borderRadius: '5px', marginBottom: '10px' }}>
                     {order.chatBox && order.chatBox.length > 0 ? (
                       order.chatBox.map((chat, index) => (
                         <div key={index} style={{ textAlign: chat.owner === 'owner' ? 'right' : 'left' }}>
@@ -217,17 +167,22 @@ const OrderDetails = () => {
                       <p>No messages</p>
                     )}
                   </div>
-                  <textarea
-                    value={newMessage}
-                    onChange={(e) => setNewMessage(e.target.value)}
-                    placeholder="Type your message here"
-                    style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px', minHeight: '100px' }}
-                  ></textarea>
-                </div>
+                  
+                <div style={{ marginBottom: '20px' }}>
+                  <div>
+                    {order.chatBox.map((chat, index) => (
+                      <div key={index} style={{ marginBottom: '10px' }}>
+                        <strong>{chat.owner}</strong>: {chat.message}
+                      </div>
+                    ))}
+                  </div>
+                  <div>
+                    <label className="order-details-label" htmlFor="newMessage">Add a message</label>
+                    <textarea className="order-details-input" name="newMessage" value={newMessage} onChange={(e) => setNewMessage(e.target.value)} style={{ width: '100%', padding: '10px', fontSize: '16px', border: '1px solid #ccc', borderRadius: '5px' }}></textarea>
+                  </div>
                 </div>
 
-
-                <button style={{ background: '#021F61', color: '#ffffff', marginLeft: '100px', marginTop: '50px' }} type="submit" className="form-button" onClick={handleSave}>
+                <button onClick={handleSave} style={{ width: '100%', padding: '10px', fontSize: '16px', backgroundColor: '#233c5c', color: '#fff', border: 'none', borderRadius: '5px', cursor: 'pointer' }}>
                   Save
                 </button>
               </div>
@@ -236,7 +191,6 @@ const OrderDetails = () => {
         </div>
       </div>
     </Helmet>
-
   );
 };
 
